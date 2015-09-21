52d91392644e4c24833034a4d5bd0686534cf047
bergh105 <bergh105@mail.chapman.edu>
2015-09-21 16:37:10 -0700

    Update UDPClient.java

--- UDPClient.java
+++ UDPClient.java
@@ -13,87 +13,83 @@ import java.net.*;
 
 class Red {
 	
-    public static void main(String args[]) throws Exception
-    {
+	public static void main(String args[]) throws Exception {
 
-      BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+		DatagramSocket clientSocket = new DatagramSocket();
 
-      DatagramSocket clientSocket = new DatagramSocket();
+		InetAddress IPAddress = InetAddress.getByName("10.49.139.94"); //server IP
 
-      InetAddress IPAddress = InetAddress.getByName("10.49.139.94"); //server IP
+		while(true){
+			byte[] sendData = new byte[1024];
+			byte[] receiveData = new byte[1024];
 
-	  while(true){
-		byte[] sendData = new byte[1024];
-		byte[] receiveData = new byte[1024];
+			String sentence = inFromUser.readLine();
+			sendData = sentence.getBytes();
+			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
 
-		String sentence = inFromUser.readLine();
-		sendData = sentence.getBytes();
-		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+			clientSocket.send(sendPacket);
 
-		clientSocket.send(sendPacket);
+			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 
-		DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+			clientSocket.receive(receivePacket);
 
-		clientSocket.receive(receivePacket);
-
-		String response = new String(receivePacket.getData());
+			String response = new String(receivePacket.getData());
 		
-		if (response.equals("100")) {
-			System.out.println("You are the first to arrive. Please wait for second user.");
-			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-			clientSocket.receive(receivePacket);
-			String response = new String(receivePacket.getData());
-			if (response.equals("200") {
-				System.out.println("Second user has connected. Send message.");
-				String sentence = inFromUser.readLine();
-				sendData = sentence.getBytes();
-				DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
-			}
-		}
-		else if (response.equals("200")) {
-			System.out.println("You are the second to arrive. Please wait for first user's message.");
-			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-			clientSocket.receive(receivePacket);
-			String response = new String(receivePacket.getData());
-			if(response.equals("300") {
+			if (response.equals("100")) {
+				System.out.println("You are the first to arrive. Please wait for second user.");
 				DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 				clientSocket.receive(receivePacket);
 				String response = new String(receivePacket.getData());
-				System.out.println(response);
+				if (response.equals("200")) {
+					System.out.println("Second user has connected. Send message.");
+					String sentence = inFromUser.readLine();
+					sendData = sentence.getBytes();
+					DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+				}
+				else {
+					System.out.println("Error receiving connection from second user.");
+					clientSocket.close();
+					break;
+				}
 			}
-		}
-		else if (response.equals("300")) {
-			System.out.println("Message sent successfully. Send another? (Type yes or no)");
-			String newSentence = inFromUser.readLine();
-			if (newSentence.equals("yes")) {
-				System.out.println("Type next message: ");
-				String sentence = inFromUser.readLine();
-				sendData = sentence.getBytes();
-				DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
-				clientSocket.send(sendPacket);
+			else if (response.equals("200")) {
+				System.out.println("You are the second to arrive. Please wait for first user's message.");
+				DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+				clientSocket.receive(receivePacket);
+				String response = new String(receivePacket.getData());
+				System.out.println(response.trim());
+			}
+			else if (response.equals("300")) {
+				System.out.println("Message sent successfully. Send another? (Type yes or no)");
+				String newSentence = inFromUser.readLine();
+				if (newSentence.equals("yes")) {
+					System.out.println("Type next message: ");
+					String sentence = inFromUser.readLine();
+					sendData = sentence.getBytes();
+					DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+					clientSocket.send(sendPacket);
+				}
+				else {
+					DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+					clientSocket.receive(receivePacket);
+					String response = new String(receivePacket.getData());
+					System.out.println(response.trim());
+				}
+			}
+			else if(response.equals("400")) {
+				System.out.println("Second user has left. Goodbye.")
+				clientSocket.close();
+				break;
 			}
 			else {
-			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-			clientSocket.receive(receivePacket);
-			String response = new String(receivePacket.getData());
-			System.out.println(response);
-			}
-		}
-		else if(response.equals("400")) {
-			System.out.println("Second user has left. Goodbye.")
-			clientSocket.close();
-			break;
-		}
-		else {
-			System.out.println(response);
-		}	
-		System.out.println("RESPONSE:" + response);
-		
+				System.out.println(response.trim());
+			}	
       
-		if (sentence.equals("Goodbye")){
-			clientSocket.close();
-			break;
-		}
-      }
+			if (sentence.equals("Goodbye")){
+				clientSocket.close();
+				break;
+			}
+      		}
 	}
 }
